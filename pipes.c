10a32ae7cbd38b10d41f4d115b27489b64e60dcc
Victor Bustamante <victorbustamante12@gmail.com>
2019-03-28 14:08:02 -0300

    Finished pipes without cli params

--- pipes.c
+++ pipes.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <math.h>
+#include <time.h>
 
 int main(int argc, char **argv){
 	
@@ -13,12 +18,46 @@ int main(int argc, char **argv){
 		return 1;
 	}
 	
-	char msg[20];
+	#define MSG_BUFSIZE 13
+	char msg[MSG_BUFSIZE];
 	if(childpid == 0){ // Is child
-		write(pipeEnd[1], "Hello\n", 7);
+		close(pipeEnd[0]);
+		int nextNum = 1;
+		srand(time(NULL));
+		for(int i = 0; i<10; i++){
+			sprintf(msg, "%d", nextNum);
+			printf("Sending: %s\n", msg);
+			write(pipeEnd[1], msg, strlen(msg) + 1);
+			sleep(1);
+			nextNum += rand() % 8;
+		}
+		printf("Sending: 0\n");
+		write(pipeEnd[1], "0\0", 2);
+		close(pipeEnd[1]);
+		printf("Encerrando escritor\n");
+
 	}else{ // Is parent
-		read(pipeEnd[0], msg, 20);
-		printf("Recv: %s", msg);
+		close(pipeEnd[1]);
+		do {
+			int size = read(pipeEnd[0], msg, MSG_BUFSIZE);
+			printf("\tRecv: %s (%d)\n", msg, size);
+			int value = atoi(msg);
+			// Check primality
+			
+			int isPrime = value >= 1?1:0;
+			for(int i = 2; i<= ceil(sqrt(value)); i++){
+				if((value % i) == 0){
+					isPrime = 0;
+					break;
+				}
+			}
+			
+			printf("\t%d %sé primo\n", value, isPrime?"":"nao ");
+			
+			
+		} while(msg[0] != '0');
+		close(pipeEnd[0]);
+		printf("Encerrando leitor\n");
 	}
 	
 	return 0;
