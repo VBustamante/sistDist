a60830404451bd43e62d2593120c57025db30bf1
Victor Bustamante <victorbustamante12@gmail.com>
2019-04-02 00:09:55 -0300

    Added parameter passing to pipes

--- pipes.c
+++ pipes.c
@@ -9,6 +9,14 @@
 
 int main(int argc, char **argv){
 	
+	if(argc != 2){
+		printf("Missing sequence size. run \"pipes $seqsize\"\n"
+		"$seqsize: Quantity of numbers to generate\n");
+		return 1;
+	}
+	int seqsize = atoi(argv[1]);
+
+	
 	int pipeEnd[2];
 	pipe(pipeEnd);
 	
@@ -24,24 +32,22 @@ int main(int argc, char **argv){
 		close(pipeEnd[0]);
 		int nextNum = 1;
 		srand(time(NULL));
-		for(int i = 0; i<10; i++){
-			sprintf(msg, "%d", nextNum);
+		for(int i = 0; i<=seqsize; i++){
+			sprintf(msg, "%d", i == seqsize?0:nextNum);
 			printf("Sending: %s\n", msg);
 			write(pipeEnd[1], msg, MSG_BUFSIZE);
 			nextNum += rand() % 8;
 		}
-		printf("Sending: 0\n");
-		write(pipeEnd[1], "0", 2);
 		close(pipeEnd[1]);
 		printf("Encerrando escritor\n");
 
 	}else{ // Is parent
 		close(pipeEnd[1]);
-		int size;
+		int value;
 		do {
-			size = read(pipeEnd[0], msg, MSG_BUFSIZE);
+			int size = read(pipeEnd[0], msg, MSG_BUFSIZE);
 			printf("\tRecv: %s (%d)\n", msg, size);
-			int value = atoi(msg);
+			value = atoi(msg);
 			// Check primality
 			
 			int isPrime = value >= 1?1:0;
@@ -55,7 +61,7 @@ int main(int argc, char **argv){
 			printf("\t%d %sé primo\n", value, isPrime?"":"nao ");
 			
 			
-		} while(msg[0] != '0' && size > 0);
+		} while(value);
 		close(pipeEnd[0]);
 		printf("Encerrando leitor\n");
 	}
