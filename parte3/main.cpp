97ca4653589596b8eb24fbf6f3d7109de52c1689
Tuniks <tonikpa@gmail.com>
2019-06-15 19:47:16 -0300

    strutctures bully algorithm

--- parte3/main.cpp
+++ parte3/main.cpp
@@ -2,15 +2,38 @@
 #include <pthread.h>
 #include <unistd.h>
 #include <semaphore.h>
+#include <time.h>
 
 using namespace std;
 
-sem_t is_sleeping_mutex, election_count_mutex, ok_count_mutex, leader_count_mutex, alive_count_mutex, alive_ok_count_mutex;
-bool is_sleeping = false;
-int election_count = 0, ok_count = 0, leader_count = 0, alive_count = 0, alive_ok_count = 0;
+sem_t is_sleeping_mutex, receive_msg_thread_mutex, check_leader_thread_mutex;
+bool is_sleeping = false, leader_checking = false, start_election_intention = false;
+int s_election_count = 0, s_ok_count = 0, s_leader_count = 0, s_alive_count = 0, s_alive_ok_count = 0;
+int r_election_count = 0, r_ok_count = 0, r_leader_count = 0, r_alive_count = 0, r_alive_ok_count = 0;
+int id, leader_id;
 
 bool leaderStatus(){
   return false;
+}
+
+string receiveMessage(){
+  return "a";
+}
+
+int getMessageType(string msg){
+  return 0;
+}
+
+int getSenderId(string msg) {
+  return 0;
+}
+
+void sendMessage(int msg){
+  return;
+}
+
+void startElection(){
+  return;
 }
 
 void *thread_ui_code(void *arg){
@@ -54,8 +77,7 @@ void *thread_ui_code(void *arg){
 
     else if(cmd == "4"){
       //print stats
-      sem_wait(&)
-
+      cout << "stats" << endl;
     }
 
     else{
@@ -68,26 +90,84 @@ void *thread_ui_code(void *arg){
 }
 
 void *thread_msgs_code(void *arg){
+  time_t start_election_time;
+  bool on_election = false;
   cout << "MSGS" << endl;
-  return nullptr;
+
+  while(true){
+    string msg = receiveMessage();
+    int type = getMessageType(msg);
+
+    //TODO MUTEX SLEEP
+    //these handlers are inactive if the process is sleeping
+    if(!is_sleeping) {
+      if (type == 0) { //received election message
+        r_election_count++; //todo mutex
+        int sender_id = getSenderId(msg);
+        if (sender_id < id){ //this process has a higher id, sends id to election starter
+          sendMessage(1);
+          s_ok_count++; //todo mutex
+          start_election_intention = true; //todo mutex
+        }
+
+      } else if (type == 1) { //received ok message
+        r_ok_count++; //todo mutex
+        on_election == false;
+
+      } else if (type == 3) { //received alive message
+        r_alive_count++; //todo mutex
+        sendMessage(4);
+        s_alive_ok_count++; //todo mutex
+
+      } else if (type == 4) { //received alive_ok message
+        r_alive_ok_count++; //todo mutex
+        leader_checking = false;  //todo mutex
+      }
+    }
+
+    //this handler still works even if the process is sleeping
+    if (type == 2) { //received leader message
+      r_leader_count++; //todo mutex
+      leader_id = getSenderId(msg);
+    }
+
+    //starts election if there is intent
+    if(start_election_intention){
+      start_election_intention = false; //todo mutex
+      //todo mandar mensagem p todo mundo
+      on_election = true;
+      start_election_time = time(NULL);
+    }
+
+    if(on_election && ((time(NULL) - start_election_time) > 10)){
+      //this process is the new leader
+      leader_id = id; //todo mutex
+      //todo avisar todo mundo
+    }
+  }
 }
 
 void *thread_leader_code(void *arg){
-
   while(true){
-    cout << "Found Leader" << endl;
+    sleep(10); //TODO ID MULTIPLIER
+    leader_checking = true; //todo mutex
+    sendMessage(3);
+    s_alive_count++;  //todo mutex
+
     sleep(10);
+    if(leader_checking){
+      leader_checking = false;  //todo mutex
+      start_election_intention = true; //todo mutex
+    } else {
+      cout << "Found Leader" << endl;
+    }
   }
-  return nullptr;
 }
 
 int main() {
   sem_init(&is_sleeping_mutex, 0, 1);
-  sem_init(&election_count_mutex, 0, 1);
-  sem_init(&ok_count_mutex, 0, 1);
-  sem_init(&leader_count_mutex, 0, 1);
-  sem_init(&alive_count_mutex, 0, 1);
-  sem_init(&alive_ok_count_mutex, 0, 1);
+  sem_init(&check_leader_thread_mutex, 0, 1);
+  sem_init(&receive_msg_thread_mutex, 0, 1);
 
   pthread_t thread_ui_id, thread_msgs_id, thread_leader_id;
 
@@ -96,17 +176,13 @@ int main() {
   pthread_create(&thread_leader_id , nullptr, thread_leader_code, nullptr);
 
   pthread_join(thread_ui_id, nullptr);
-  pthread_join(thread_msgs_id, nullptr);
 
   pthread_cancel(thread_msgs_id);
   pthread_cancel(thread_leader_id);
 
   sem_destroy(&is_sleeping_mutex);
-  sem_destroy(&election_count_mutex);
-  sem_destroy(&ok_count_mutex);
-  sem_destroy(&leader_count_mutex);
-  sem_destroy(&alive_count_mutex);
-  sem_destroy(&alive_ok_count_mutex);
+  sem_destroy(&receive_msg_thread_mutex);
+  sem_destroy(&check_leader_thread_mutex);
 
   return 0;
 }
